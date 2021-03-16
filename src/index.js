cd3d7c9c809cb13c34afbc3e5168f4df6279f0b8
ekaterinalushnikova <lushkat18@gmail.com>
2021-03-16 20:18:39 +0300

    fix: task js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
+  if (typeof array == "underfined" || array.length == 0)
+  {
   return 0;
+  }
+return Math.min.apply (0, array);
 }
 
+
 exports.max = function max (array) {
+  if (typeof array == "underfined" || array.length == 0)
+  {
   return 0;
+  }
+  return Math.max.apply (0, array);
 }
 
 exports.avg = function avg (array) {
+  if (typeof array == "underfined" || array.length == 0)
+  {
   return 0;
+  }
+  let average = 0;
+  for (x=0; x < array.length; x++) {
+    average += array [x];
+  }
+  return average/array.length;
 }
